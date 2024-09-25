d7380b20e3d7b286b9cd29f3ae1296ea755a6426
Alper Ozgur Sahin <alperozgursahin6@gmail.com>
2024-09-25 16:29:54 +0300

    Refactor query and state handling in car garage application & Tests

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,44 +1,125 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Lib2
     ( Query(..),
-    parseQuery,
-    State(..),
-    emptyState,
-    stateTransition
+      parseQuery,
+      State(..),
+      emptyState,
+      stateTransition,
+      Car(..)        -- Car data type exporting
     ) where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
+-- | An entity which represents user input.
+-- It defines different types of commands that can be parsed.
 data Query
+    = Add Car Query    -- Recursive Add command with a single car and a continuation
+    | Remove Int       -- Remove command with an id
+    | List             -- List command
+    | Quit             -- Quit command
+    deriving (Eq, Show)
 
--- | The instances are needed basically for tests
-instance Eq Query where
-  (==) _ _= False
-
-instance Show Query where
-  show _ = ""
+-- | A car data type to represent a car's details.
+data Car = Car
+    { make  :: String   -- The make of the car
+    , model :: String   -- The model of the car
+    , year  :: Int      -- The year of manufacture
+    } deriving (Eq, Show)
 
 -- | Parses user's input.
--- The function must have tests.
+-- It takes a string input and converts it into a Query type.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery input =
+    case words input of
+        ("add":rest) -> parseAdd (unwords rest)  -- Parse add command with remaining string
+        ["list"]     -> Right List                -- Parse list command
+        ["remove", n] | all (`elem` "0123456789") n -> Right (Remove (read n))  -- Parse remove command
+        ["quit"]     -> Right Quit                -- Parse quit command
+        _            -> Left "Unknown command"    -- Handle unknown commands
+
+-- Helper function to parse add command.
+-- It processes a string input and splits it into individual car entries.
+parseAdd :: String -> Either String Query
+parseAdd [] = Left "No cars to add."            -- Handle empty input
+parseAdd carsInput =
+    let cars = splitByComma carsInput
+    in if null cars
+       then Left "No cars to add."                -- Handle case with no cars
+       else parseCars cars                         -- Parse the list of cars
+
+-- Custom function to split input string by comma.
+-- This function splits the input based on commas.
+splitByComma :: String -> [String]
+splitByComma [] = []
+splitByComma xs = let (first, rest) = break (== ',') xs
+                  in first : case rest of
+                                [] -> []
+                                _:ys -> splitByComma (dropWhile (== ' ') ys)
+
+-- Recursive function to parse multiple cars.
+-- This function processes a list of car strings and converts them into Add commands.
+parseCars :: [String] -> Either String Query
+parseCars [] = Left "No cars to add."          -- Handle case with no cars
+parseCars [carStr] = Right $ Add (parseCar carStr) Quit -- End of recursion
+parseCars (carStr:rest) = do
+    let nextQuery = parseCars rest               -- Recursively parse the rest of the cars
+    case nextQuery of
+        Left err -> Left err                      -- Propagate errors
+        Right q  -> Right $ Add (parseCar carStr) q
+
+-- Helper to parse a car (make, model, year).
+-- It converts a string representation of a car into a Car data type.
+parseCar :: String -> Car
+parseCar carStr =
+    let parts = words carStr
+    in case parts of
+        [make, model, yearStr] -> Car make model (read yearStr)  -- Construct Car instance
+        _                      -> error "Invalid car format"       -- Handle invalid format
 
 -- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+-- It contains the list of cars currently stored in the program.
+data State = State
+    { cars :: [Car]   -- List of cars in the garage
+    } deriving (Eq, Show)
 
 -- | Creates an initial program's state.
--- It is called once when the program starts.
+-- This function initializes the state with an empty list of cars.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State { cars = [] }
 
 -- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
+-- This function takes the current state and a user command and updates the state accordingly.
+-- Right contains an optional message to print and an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition state (Add newCar nextQuery) =
+    let newState = state { cars = cars state ++ [newCar] }  -- Add new car to the list
+    in case nextQuery of
+        Quit -> Right (Just "Cars added.", newState)         -- If Quit, return message
+        _    -> stateTransition newState nextQuery           -- Continue processing next queries
+
+-- Handle List command.
+stateTransition state List =
+    if null (cars state)
+    then Right (Just "No cars available.", state)          -- Return message if no cars
+    else 
+        let carList = unlines $ zipWith (\i car -> show i ++ ": " ++ show car) [1..] (cars state)
+            trimmedCarList = if null carList then "" else init carList  -- Remove trailing newline
+        in Right (Just trimmedCarList, state)               -- Return the list of cars
+
+-- Handle Remove command.
+stateTransition state (Remove idx) =
+    if idx > 0 && idx <= length (cars state)
+    then
+        let newState = state { cars = take (idx - 1) (cars state) ++ drop idx (cars state) }
+        in Right (Just "Car removed.", newState)            -- Car successfully removed
+    else 
+        Right (Just "Invalid car ID.", state)              -- Return error for invalid ID
+
+-- Handle Quit command.
+stateTransition state Quit =
+    Right (Just "Goodbye!", state)                         -- Return goodbye message
+
+-- | Helper function to split list into chunks of size n.
+-- This function splits a list into smaller lists of a specified size.
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs : chunksOf n (drop n xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 import Lib1 qualified
 import Lib2 qualified
@@ -9,14 +10,57 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [unitTests, lib2Tests]
 
 unitTests :: TestTree
 unitTests = testGroup "Lib1 tests"
   [ testCase "List of completions is not empty" $
       null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
+    testCase "Parsing case 1 - empty input" $
+      Lib2.parseQuery "" @?= Left "Unknown command",
+    testCase "Parsing case 2 - single character" $
+      Lib2.parseQuery "o" @?= Left "Unknown command",
+    testCase "Parsing case 3 - unknown command" $
+      Lib2.parseQuery "unknown" @?= Left "Unknown command",
+    testCase "Parsing case 4 - valid add command" $
+      Lib2.parseQuery "add Toyota Camry 2020" @?= Right (Lib2.Add (Lib2.Car "Toyota" "Camry" 2020) Lib2.Quit)
+  ]
+
+lib2Tests :: TestTree
+lib2Tests = testGroup "Lib2 tests"
+  [ testCase "Parse add command with one car" $
+      Lib2.parseQuery "add Toyota Camry 2020" @?= Right (Lib2.Add (Lib2.Car "Toyota" "Camry" 2020) Lib2.Quit),
+
+    testCase "Parse add command with multiple cars" $
+      Lib2.parseQuery "add Toyota Camry 2020, Honda Civic 2021" @?= Right (Lib2.Add (Lib2.Car "Toyota" "Camry" 2020) (Lib2.Add (Lib2.Car "Honda" "Civic" 2021) Lib2.Quit)),
+
+    testCase "Parse remove command" $
+      Lib2.parseQuery "remove 1" @?= Right (Lib2.Remove 1),
+
+    testCase "Parse list command" $
+      Lib2.parseQuery "list" @?= Right Lib2.List,
+
+    testCase "Parse quit command" $
+      Lib2.parseQuery "quit" @?= Right Lib2.Quit,
+
+    testCase "State transition add multiple cars" $
+      Lib2.stateTransition Lib2.emptyState (Lib2.Add (Lib2.Car "Toyota" "Camry" 2020) (Lib2.Add (Lib2.Car "Honda" "Civic" 2021) Lib2.Quit)) @?= Right (Just "Cars added.", Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]),
+
+    testCase "State transition list cars after adding multiple" $
+      Lib2.stateTransition (Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]) Lib2.List @?= Right (Just "1: Car {make = \"Toyota\", model = \"Camry\", year = 2020}\n2: Car {make = \"Honda\", model = \"Civic\", year = 2021}", Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]),
+
+    testCase "State transition remove car" $
+      Lib2.stateTransition (Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]) (Lib2.Remove 1) @?= Right (Just "Car removed.", Lib2.State [Lib2.Car "Honda" "Civic" 2021]),
+
+    testCase "Parse invalid remove command (non-existent ID)" $
+      Lib2.stateTransition (Lib2.State [Lib2.Car "Toyota" "Camry" 2020]) (Lib2.Remove 99) @?= Right (Just "Invalid car ID.", Lib2.State [Lib2.Car "Toyota" "Camry" 2020]),
+
+    testCase "State transition list cars with no cars" $
+      Lib2.stateTransition Lib2.emptyState Lib2.List @?= Right (Just "No cars available.", Lib2.emptyState),
+
+    testCase "Parse list command after adding cars" $
+      Lib2.stateTransition (Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]) Lib2.List @?= Right (Just "1: Car {make = \"Toyota\", model = \"Camry\", year = 2020}\n2: Car {make = \"Honda\", model = \"Civic\", year = 2021}", Lib2.State [Lib2.Car "Toyota" "Camry" 2020, Lib2.Car "Honda" "Civic" 2021]),
+
+    testCase "State transition quit command" $
+      Lib2.stateTransition Lib2.emptyState Lib2.Quit @?= Right (Just "Goodbye!", Lib2.emptyState)
   ]
